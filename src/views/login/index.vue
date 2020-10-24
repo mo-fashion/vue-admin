c554883cbebd162d18ae8db7a43b77ad94ef1a2d
Makeavow <1192940849@qq.com>
2020-10-24 11:53:29 +0800

    vue3.0基础框架搭成

---
+++ src/views/login/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="login">
+    <h1>登录页1233</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "index"
+};
+</script>
+
+<style lang="scss" scoped>
+#login {
+  color: #110308;
+}
+</style>
--- vue.config.js
+++ vue.config.js
@@ -1,59 +1,58 @@
-const path = require('path');
+const path = require("path");
 module.exports = {
-    //基本路径
-    publicPath: process.env.NODE_ENV === 'production' ? '' : '/',
-    //输出文件目录
-    outputDir: process.env.NODE_ENV === 'production' ? 'dist' : 'devdist',
-    //eslint-loader  是否在保存的时候检查
-    lintOnSave: true,
-    /**
-     *	webpack配置，see https://github.com/vuejs/vue-cli/blob/dev/docs/webpack.md
-     **/
-    chainWebpack: (config) => {
+  //基本路径
+  publicPath: process.env.NODE_ENV === "production" ? "" : "/",
+  //输出文件目录
+  outputDir: process.env.NODE_ENV === "production" ? "dist" : "devdist",
+  //eslint-loader  是否在保存的时候检查
+  lintOnSave: true,
+  /**
+   *	webpack配置，see https://github.com/vuejs/vue-cli/blob/dev/docs/webpack.md
+   **/
+  chainWebpack: config => {},
+  configureWebpack: config => {},
+  // 生产环境是否生成 sourceMap 文件
+  productionSourceMap: false,
+  // css相关配置
+  css: {
+    // 是否使用css分离插件 ExtractTextPlugin
+    // extract: true,
+    //开启 CSS source maps?
+    sourceMap: false,
+    //css预设器配置项
+    loaderOptions: {
+      sass: {
+        prependData: `@import "./src/styles/main.scss";`
+      }
     },
-    configureWebpack: (config) => {
+    //启用 CSS modules for all css / pre-processor files.
+    // modules: false
+    requireModuleExtension: true
+  },
+  parallel: require("os").cpus().length > 1,
+  /**
+   * PWA 插件相关配置，see https://github.com/vuejs/vue-cli/blob/dev/packages/%40vue/cli-plugin-pwa
+   **/
+  pwa: {},
+  // webpack-dev-server 相关配置
+  devServer: {
+    open: false, //编译完成是否打开网页
+    host: "0.0.0.0", //指定使用地址，默认localhost， 0.0.0.0代表可以被外键访问
+    port: 8080, //访问断开
+    disableHostCheck: true,
+    https: false, //编译失败是刷新页面
+    hot: true, //开启热加载
+    // hostOnly: false,
+    proxy: null, //设置代理
+    overlay: {
+      //全屏模式下是否显示脚本错误
+      warnings: true,
+      errors: true
     },
-    // 生产环境是否生成 sourceMap 文件
-    productionSourceMap: false,
-    // css相关配置
-    css: {
-        // 是否使用css分离插件 ExtractTextPlugin
-        extract: true,
-        //开启 CSS source maps?
-        sourceMap: false,
-        //css预设器配置项
-        loaderOptions: {
-            sass: {
-                prependData: `@import "./src/styles/main.scss";`
-            }
-        },
-        //启用 CSS modules for all css / pre-processor files.
-        // modules: false
-        requireModuleExtension: true
-    },
-    parallel: require('os').cpus().length > 1,
-    /**
-     * PWA 插件相关配置，see https://github.com/vuejs/vue-cli/blob/dev/packages/%40vue/cli-plugin-pwa
-     **/
-    pwa: {},
-    // webpack-dev-server 相关配置
-    devServer: {
-        open: false, //编译完成是否打开网页
-        host: '0.0.0.0', //指定使用地址，默认localhost， 0.0.0.0代表可以被外键访问
-        port: 8080, //访问断开
-        https: false, //编译失败是刷新页面
-        hot: true, //开启热加载
-        // hostOnly: false,
-        proxy: null, //设置代理
-        overlay: { //全屏模式下是否显示脚本错误
-            warnings: true,
-            errors: true
-        },
-        before: app => {
-        }
-    },
-    /**
-     * 第三方插件配置
-     **/
-    pluginOptions: {}
-}
+    before: app => {}
+  },
+  /**
+   * 第三方插件配置
+   **/
+  pluginOptions: {}
+};
